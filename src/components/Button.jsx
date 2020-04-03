5a2e78fee9715e0f306ef9e8117a7ca2ee5536f0
mesajoao91 <mesajoao@gmail.com>
2020-04-03 09:21:37 -0500

    Nuevos componentes y Fake API Actualizada

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
 
-class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        count: 0,
-      }
-    }
-  }
-  handelClick = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  }
+const Button = (props) => {
+  const { text } = props;
+  return (
+    <div>
+      <button type='button'>{props.text}</button>
+      <button type='button'>{text}</button>
+    </div>
+  );
+};
 
-  render() {
-    const { count } = this.state;
-    return (
-      <div>
-        <h1>
-          Manzanas:
-          {count}
-        </h1>
-        <button type='button' onClick={this.handelClick}>Agregar</button>
-      </div>
-    );
-  }
-}
+// class Button extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//         count: 0,
+//       }
+//     }
+//   }
+//   handelClick = () => {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   }
+
+//   render() {
+//     const { count } = this.state;
+//     return (
+//       <div>
+//         <h1>
+//           Manzanas:
+//           {count}
+//         </h1>
+//         <button type='button' onClick={this.handelClick}>Agregar</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default Button;
